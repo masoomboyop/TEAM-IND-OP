b08065d2cc77eebcd7c930e9194c8ddcf398b169
fullmojmasti <104665316+fullmojmasti@users.noreply.github.com>
2022-07-17 00:51:24 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -294,13 +294,41 @@ async def play(_, message: Message):
             views = "NaN"
             keyboard = InlineKeyboardMarkup(
             [
-                [
-                        InlineKeyboardButton(
-                            text="💥 Jσιɳ Oυɾ Cԋαƚ Gɾσυρ 💞",
-                            url=f"https://t.me/FULL_ON_MOJJ_MASTI")
-
-                ]
-            ]
+[
+            InlineKeyboardButton(
+                text=f"{played} •{bar}• {dur}",
+                callback_data="GetTimer",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="▷",
+                callback_data=f"ADMIN Resume|{chat_id}",
+            ),
+            InlineKeyboardButton(
+                text="II", callback_data=f"ADMIN Pause|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+            ),
+            InlineKeyboardButton(
+                text="▢", callback_data=f"ADMIN Stop|{chat_id}"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="✯ ᴍᴇɴᴜ ✯",
+                callback_data=f"PanelMarkup {videoid}|{chat_id}",
+            ),
+            InlineKeyboardButton(
+                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"https://t.me/FULL_ON_MOJJ_MASTI"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSEMENU_BUTTON"], callback_data="close"
+            )
+        ],
         )
 
         if (dur / 60) > DURATION_LIMIT:
