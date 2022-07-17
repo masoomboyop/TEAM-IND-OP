1221e77f6a16fee676759f94c5f5c3e5a1f06647
fullmojmasti <104665316+fullmojmasti@users.noreply.github.com>
2022-07-17 08:58:44 +0530

    Update player.py

--- plugins/player.py
+++ plugins/player.py
@@ -294,7 +294,7 @@ async def play(_, message: Message):
             views = "NaN"
             keyboard = InlineKeyboardMarkup(
             [
-[
+(
             InlineKeyboardButton(
                 text=f"{played} •{bar}• {dur}",
                 callback_data="GetTimer",
@@ -328,7 +328,7 @@ async def play(_, message: Message):
             InlineKeyboardButton(
                 text=_["CLOSEMENU_BUTTON"], callback_data="close"
             )
-        ],
+            ),
         )
 
         if (dur / 60) > DURATION_LIMIT:
